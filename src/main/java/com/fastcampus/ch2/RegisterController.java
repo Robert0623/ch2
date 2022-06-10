30ab0dd9f8d50b2e081cc13463cb17759bc7781c
Robert0623 <tone8943@gmail.com>
2022-06-10 11:10:36 +0900

    ch2-30 WebDataBinder, 데이터 변환

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -2,27 +2,45 @@ package com.fastcampus.ch2;
 
 import java.net.URLEncoder;
 
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-//	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
-//	@RequestMapping("/register/add")
+	
+	@InitBinder
+	public void toDate(WebDataBinder binder) {
+		ConversionService conversionService = binder.getConversionService();
+		System.out.println("conversionService="+conversionService);
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+		binder.registerCustomEditor(String[].class, "hobby", new StringArrayPropertyEditor("#"));
+	}
+	
+	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
 //	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm";
-//	}
+	public String register() {
+		return "registerForm";
+	}
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save") //spring 4.3부터
-	public String save(User user, Model m) throws Exception {
+	public String save(User user, BindingResult result, Model m) throws Exception {
+		System.out.println("result="+result);
+		System.out.println("user="+user);
 		//1. 유효성 검사
 		if(!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
 			
 			m.addAttribute("msg", msg);
-			return "redirect:/register/add"; 
+			return "forward:/register/add"; 
 //			return "redirect:/register/add?msg="+msg; //URL재작성
 		}
 		//2. DB에 신규회원 정보를 저장
@@ -31,7 +49,7 @@ public class RegisterController {
 
 private boolean isValid(User user) {
 	// TODO Auto-generated method stub
-	return false;
+	return true;
 }
 
 }
